17155b8c6eebf184c31c057a199653dbca954641
junior.sulbaran <jasulbaran@gmail.com>
2024-10-21 09:05:52 -0400

    update main
    se agrego salto de linea 18

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from letraRandom import cantarLetra
 import threading
 import pytz
 from datetime import datetime, timedelta
+
 #variables sorteo letra
 sorteoletra = ''
 # Definir la zona horaria de Venezuela
