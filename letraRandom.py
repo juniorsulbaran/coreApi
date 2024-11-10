109e4825e9726f02761544d368c541ee0845ee7e
junior.sulbaran <jasulbaran@gmail.com>
2024-11-10 08:14:41 -0400

    obtimizacion del codigo
    se optimizo el codigo del sorteo letra

--- letraRandom.py
+++ letraRandom.py
@@ -1,116 +1,32 @@
 import random
-#Generamos aleatorios
+# Variable para almacenar el último número generado
+ultimo_numero = None
+
+# Generamos aleatorios
 def cantarLetra():
-    aleatorios = random.sample(range(1,28),1)
-    #numero letra B
-    if aleatorios[0] >= 1 and aleatorios[0] <= 27:
-        if aleatorios[0]== 1:
-            letra = "A"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 2:
-            letra = "B"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 3:
-            letra = "C"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 4:
-            letra = "D"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 5:
-            letra = "E"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 6:
-            letra = "F"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 7:
-            letra = "G"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 8:
-            letra = "H"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 9:
-            letra = "I"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 10:
-            letra = "J"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 11:
-            letra = "K"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 12:
-            letra = "L"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 13:
-            letra = "M"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 14:
-            letra = "N"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 15:
-            letra = "Ñ"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 16:
-            letra = "O"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 17:
-            letra = "P"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 18:
-            letra = "Q"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 19:
-            letra = "R"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 20:
-            letra = "S"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 21:
-            letra = "T"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 22:
-            letra = "U"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 23:
-            letra = "V"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 24:
-            letra = "W"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 25:
-            letra = "X"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 26:
-            letra = "Y"
-            print("Letra Premiada: ", letra)
-            return letra
-        if aleatorios[0]== 27:
-            letra = "Z"
-            print("Letra Premiada: ", letra)
-            return letra
+    global ultimo_numero  # Usamos la variable global para acceder al último número
+
+    # Generar un nuevo número aleatorio que no sea igual al último
+    while True:
+        aleatorios = random.sample(range(1, 28), 1)[0]  # Genera un número aleatorio entre 1 y 27
+        if aleatorios != ultimo_numero:  # Verifica que no sea igual al último número
+            ultimo_numero = aleatorios  # Actualiza el último número
+            break  # Sale del bucle si se genera un número válido
+
+    # Mapeo de números a letras
+    letras = {
+        1: "A", 2: "B", 3: "C", 4: "D", 5: "E",
+        6: "F", 7: "G", 8: "H", 9: "I", 10: "J",
+        11: "K", 12: "L", 13: "M", 14: "N", 15: "Ñ",
+        16: "O", 17: "P", 18: "Q", 19: "R", 20: "S",
+        21: "T", 22: "U", 23: "V", 24: "W", 25: "X",
+        26: "Y", 27: "Z"
+    }
+
+    # Obtener la letra correspondiente al número generado
+    letra = letras.get(aleatorios)
+    print("letra :", ultimo_numero)
+    print("Letra Premiada:", letra)
+    return letra
         
         
